dc858104214d2d32fe5f78dca5ceab8c2dc9f1fa
sarahc987 <89328572+sarahc987@users.noreply.github.com>
2021-10-14 10:09:50 +1000

    Linked new forecast API to JS

--- src/index.js
+++ src/index.js
@@ -11,6 +11,12 @@ function currentTime(timestamp) {
   return `${hours}:${minutes}`;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "3def0561d3e06af25dd72c96c23230ff";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showWeather(response) {
   let currentWeather = document.querySelector(".currentWeather");
   currentWeather.innerHTML = response.data.weather[0].description;
@@ -32,7 +38,7 @@ function showWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   weatherIcon.setAttribute("alt", response.data.weather[0].description);
-  displayForecast();
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -64,7 +70,8 @@ function fahrenConvert(event) {
   temperatureElement.innerHTML = Math.round(fahrenheitTemp);
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
